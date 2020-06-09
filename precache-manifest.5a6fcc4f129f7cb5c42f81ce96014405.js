962594b332e12e608a099d0d93d3a68b8853e7a2
Nimrockdev <nimrockdev@gmail.com>
2020-06-09 17:08:49 +0200

    Updates

---
+++ precache-manifest.5a6fcc4f129f7cb5c42f81ce96014405.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1430c7c327b3a2db6f51c67f9f80ec45",
+    "url": "/reactTicTacToe/index.html"
+  },
+  {
+    "revision": "27b6cb017a42230ba2f2",
+    "url": "/reactTicTacToe/static/css/main.4d3a117d.chunk.css"
+  },
+  {
+    "revision": "32540ac14b0f9e67caa5",
+    "url": "/reactTicTacToe/static/js/2.ac17b5d9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactTicTacToe/static/js/2.ac17b5d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27b6cb017a42230ba2f2",
+    "url": "/reactTicTacToe/static/js/main.fd1386d7.chunk.js"
+  },
+  {
+    "revision": "65ebe6a00460f5be138e",
+    "url": "/reactTicTacToe/static/js/runtime-main.424b4423.js"
+  }
+]);
